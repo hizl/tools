dea96f5f4fd270b541a1643a347eaedc01657e74
dev <hizlle1817@gmail.com>
2021-04-03 14:56:42 +0400

    update

--- src/main/java/investment/tools/entity/Dividends.java
+++ src/main/java/investment/tools/entity/Dividends.java
@@ -4,33 +4,41 @@ package investment.tools.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+
 
 
 @Entity
 @Table(name = "DIVIDENDS")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor()
 public class Dividends {
+
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
 
-    @Column
+    @Column(name = "my_current_date")
     private String my_current_date;
 
-    @Column
+    @Column(name = "my_current_company")
     private String my_current_company;
 
-    @Column
+    @Column(name = "my_current_price")
     private String my_current_price;
 
-
-
-
-
-
-
-
-
+    public Dividends(String my_current_date, String my_current_company, String my_current_price) {
+        this.my_current_date = my_current_date;
+        this.my_current_company = my_current_company;
+        this.my_current_price = my_current_price;
+    }
 }
--- src/main/java/investment/tools/service/DividendService.java
+++ src/main/java/investment/tools/service/DividendService.java
@@ -12,4 +12,6 @@ public interface DividendService {
 
     List<DividendsDto> findAll();
 
+
+    DividendsDto findById(Integer id);
 }
