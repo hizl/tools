89a32cfaba3431b990aca2b97bbc7bdfcbc82e07
dev <hizlle1817@gmail.com>
2021-04-07 17:03:36 +0400

    update

--- src/main/java/investment/tools/Hibirnate/BiDirectional.java
+++ src/main/java/investment/tools/Hibirnate/BiDirectional.java
@@ -1,7 +1,43 @@
 package investment.tools.Hibirnate;
 
+import investment.tools.entity.ContactDetails;
+import org.hibernate.Session;
+
 public class BiDirectional {
     public static void main(String[] args) {
+    BIDIRECTIONALRELATION();
+    }
+
+
+    public static void BIDIRECTIONALRELATION() {
+        HibernateConnector.getSessionFactoryWithContactDetails();
+
+
+        try {
+
+
+            Session session = HibernateConnector.getSessionFactoryWithContactDetails().getCurrentSession();
+            //create entity
+
+            session.beginTransaction();
+            ContactDetails contactDetails = new ContactDetails("BiDirectional",
+                    "second_contact");
+
+            session.save(contactDetails);
+
+            session.getTransaction().commit();
+
+
+
+        } finally {
+            HibernateConnector.closeSessionFactory();
+
+
+
+        }
+
 
     }
+
+
 }
